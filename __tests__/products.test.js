bfcbb04c71542565ff0f9f95b95199d44952d6de
Sofia Premuzaj <sofia.premuzaj@gmail.com>
2021-11-18 11:06:06 +0100

    added storage and users.json

--- __tests__/products.test.js
+++ __tests__/products.test.js
@@ -3,6 +3,7 @@ import request from "supertest"
 
 describe("products", () => {
   it("get products", async () => {
-    const res = await request.get("/products")
+    const res = await request(products).get("/products")
+    expect(res.statusCode).toBe(200)
   })
 })
