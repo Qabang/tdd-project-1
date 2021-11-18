109d923b366a550cbd1ac2c13f14c40ae18871b8
NiclasLundstrom <lille_niclas@hotmail.com>
2021-11-18 13:02:57 +0100

    modified products to se content

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,10 @@
 import express from 'express'
+import products from '../storage/products.js'
 const router = express.Router()
 
 // Landingpage.
 router.get('/products', async (req, res) => {
-  res.send('hello world')
+  res.send(products)
 })
 
 export default router
