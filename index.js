4a8eb1516fca30302aaab073574fd10903fd04a5
Sandra Lindström <sandra.lindstrm@gmail.com>
2021-11-23 12:46:45 +0100

    Removed .listen and db connection from index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import products from './routes/products.js'
-import mongoose from 'mongoose'
 
 const app = express()
 app.use(express.urlencoded({ extended: true }))
@@ -10,18 +9,5 @@ app.use('/products', products)
 app.get('/', (req, res) => {
   res.send('hej')
 })
-
-mongoose.connect(
-  'mongodb+srv://order:order123@cluster0.q5oir.mongodb.net/TDD?retryWrites=true&w=majority',
-  () => console.log('connected')
-)
-
-const db = mongoose.connection
-db.on('error', console.error.bind(console, 'connection error: '))
-db.once('open', function () {
-  console.log('Connected successfully')
-})
-
-app.listen(3000, () => {})
 
 export default app
